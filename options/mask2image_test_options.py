c45d665fc571d72d16050f77b7419e695251b4af
Xinchen Yan <xcyan@umich.edu>
2019-02-16 01:52:19 -0500

    added test scripts city

--- options/mask2image_test_options.py
+++ options/mask2image_test_options.py
@@ -1,12 +1,12 @@
 ### Copyright (C) 2017 NVIDIA Corporation. All rights reserved. 
 ### Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode).
-from mask2image_test_options import MaskToImageOptions
+from mask2image_base_options import MaskToImageOptions
 
 class MaskToImageTestOptions(MaskToImageOptions):
     def initialize(self):
         MaskToImageOptions.initialize(self)
         self.parser.add_argument('--ntest', type=int, default=float("inf"), help='# of test examples.')
-        self.parser.add_argument('--results_dir', type=str, default='./results/', help='saves results here.')
+        self.parser.add_argument('--results_dir', type=str, default='./checkpoints/', help='saves results here.')
         self.parser.add_argument('--aspect_ratio', type=float, default=1.0, help='aspect ratio of result images')
         self.parser.add_argument('--phase', type=str, default='test', help='train, val, test, etc')
         self.parser.add_argument('--which_epoch', type=str, default='latest', help='which epoch to load? set to latest to use latest cached model')
